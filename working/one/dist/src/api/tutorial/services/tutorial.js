1d9585de599c0e52fda54337eadd6089d4bd06d0
Alex Vyber <alexvyber@gmail.com>
2022-08-19 20:52:42 +0300

    finnished 3 chapter

---
+++ working/one/dist/src/api/tutorial/services/tutorial.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * tutorial service.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::tutorial.tutorial');
